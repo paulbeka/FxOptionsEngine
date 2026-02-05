4cdc71e349d7c016e9382f84b32f10fed6afde26
Paul Bekaert <2517073B@student.gla.ac.uk>
2026-02-05 22:00:31 +0100

    Working API baseline

---
+++ FxOptionsEngine.Api/Controllers/SabrController .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FxOptionsEngine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/sabr")]
+    public class SabrController : ControllerBase
+    {
+        [HttpGet("volatility")]
+        public IActionResult GetVolatility(
+            [FromQuery] double forward,
+            [FromQuery] double strike
+        )
+        {
+
+            return Ok(new
+            {
+                Forward = forward,
+                Strike = strike,
+                Volatility = 0.085 
+            });
+        }
+    }
+}
